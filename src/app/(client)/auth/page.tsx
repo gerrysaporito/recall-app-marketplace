198909ee59ed3f1e42b535ea7ba161624f0d227f
Gerry Saporito <gerrysaporito@gmail.com>
2024-11-17 22:04:07 -0500

    working

--- src/app/(client)/auth/page.tsx
+++ src/app/(client)/auth/page.tsx
@@ -1,6 +1,15 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/config/nextAuth";
 import { AuthCard } from './_components/AuthCard';
 
-export default function AuthPage() {
+export default async function AuthPage() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <main className="min-h-screen flex items-center justify-center p-4">
       <AuthCard />
